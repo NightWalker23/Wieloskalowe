3d387941b3323e6d841b2e8b50264f75cc04a31e
NightWalker23 <taptnw@gmail.com>
2018-05-12 13:00:49 +0200

    dodano pentagonalne i heksagonalne sasiedztwo

--- CellularAutomationGUI/src/model/ModelGrainGrowth.java
+++ CellularAutomationGUI/src/model/ModelGrainGrowth.java
@@ -1,11 +1,11 @@
 package model;
 
+import java.util.*;
+
 import javafx.scene.paint.Color;
 import model.cells.CellGrain;
 
 import static model.cells.CellGrain.*;
-
-import java.util.*;
 
 public class ModelGrainGrowth {
     private CellGrain[][] grid;
@@ -13,7 +13,6 @@ public class ModelGrainGrowth {
     private NeighborhoodType neighborhoodType;
     private EdgeType edgeType;
     private List<GrainType> listOfGrains;
-    private Map<Integer, Integer> grainMap;
 
     public enum EdgeType {
         Closed, Periodic
@@ -49,12 +48,12 @@ public class ModelGrainGrowth {
         fillRandomly(grainAmount);
     }
 
-    private void fillMap(int id) {
+    private void fillMap(int id, Map<Integer, Integer> grainMap) {
         if (grainMap.containsKey(id)) grainMap.put(id, grainMap.get(id) + 1);
         else grainMap.put(id, 1);
     }
 
-    private int getIDMaxNeighbour() {
+    private int getIDMaxNeighbour(Map<Integer, Integer> grainMap) {
         Map.Entry<Integer, Integer> maxEntry = null;
 
         for (Map.Entry<Integer, Integer> entry : grainMap.entrySet())
@@ -65,180 +64,235 @@ public class ModelGrainGrowth {
         if (maxEntry != null) {
             int max = maxEntry.getValue();
 
-            int numberOfMaxs = 0;
+            List<Integer> listMax = new ArrayList<>();
             for (Map.Entry<Integer, Integer> entry : grainMap.entrySet())
                 if (entry.getValue() == max)
-                    numberOfMaxs++;
-
-            int[] tabMaxs = new int[numberOfMaxs];
-            int i = 0;
-            for (Map.Entry<Integer, Integer> entry : grainMap.entrySet())
-                if (entry.getValue() == max)
-                    tabMaxs[i++] = entry.getKey();
+                    listMax.add(entry.getKey());
 
             Random rand = new Random();
-            int randWinner = rand.nextInt(numberOfMaxs);
-            id = tabMaxs[randWinner];
+            int randWinner = rand.nextInt(listMax.size());
+            id = listMax.get(randWinner);
         }
 
         return id;
     }
 
-    private int vonNeuman(CellGrain[][] frame, int height, int width) {
-        grainMap = new HashMap<>();
+    private int vonNeuman(CellGrain[][] frame, int iG, int i, int iD, int jL, int j, int jR) {
+        Map<Integer, Integer> grainMap = new HashMap<>();
         int newID = 0;
-        int iG, i, iD, jL, j, jR;
-        i = height;
-        j = width;
-
-        if (height == 0)
-            if (edgeType == EdgeType.Closed) iG = -1;
-            else iG = gridHeight - 1;
-        else iG = height - 1;
-
-        if (height == gridHeight - 1)
-            if (edgeType == EdgeType.Closed) iD = -1;
-            else iD = 0;
-        else iD = height + 1;
-
-        if (width == 0)
-            if (edgeType == EdgeType.Closed) jL = -1;
-            else jL = gridWidth - 1;
-        else jL = width - 1;
-
-        if (width == gridWidth - 1)
-            if (edgeType == EdgeType.Closed) jR = -1;
-            else jR = 0;
-        else jR = width + 1;
-
         int grainType;
 
         if (iG != -1)
             if (frame[iG][j].getState() == State.GRAIN) {
                 grainType = frame[iG][j].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (jL != -1)
             if (frame[i][jL].getState() == State.GRAIN) {
                 grainType = frame[i][jL].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (jR != -1)
             if (frame[i][jR].getState() == State.GRAIN) {
                 grainType = frame[i][jR].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (iD != -1)
             if (frame[iD][j].getState() == State.GRAIN) {
                 grainType = frame[iD][j].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
-        int max = getIDMaxNeighbour();
+        int max = getIDMaxNeighbour(grainMap);
         newID = max;
 
         return newID;
     }
 
-    private int Moore(CellGrain[][] frame, int height, int width) {
-        grainMap = new HashMap<>();
+    private int Moore(CellGrain[][] frame, int iG, int i, int iD, int jL, int j, int jR) {
+        Map<Integer, Integer> grainMap = new HashMap<>();
         int newID = 0;
-
-        int iG, i, iD, jL, j, jR;
-        i = height;
-        j = width;
-
-        if (height == 0)
-            if (edgeType == EdgeType.Closed) iG = -1;
-            else iG = gridHeight - 1;
-        else iG = height - 1;
-
-        if (height == gridHeight - 1)
-            if (edgeType == EdgeType.Closed) iD = -1;
-            else iD = 0;
-        else iD = height + 1;
-
-        if (width == 0)
-            if (edgeType == EdgeType.Closed) jL = -1;
-            else jL = gridWidth - 1;
-        else jL = width - 1;
-
-        if (width == gridWidth - 1)
-            if (edgeType == EdgeType.Closed) jR = -1;
-            else jR = 0;
-        else jR = width + 1;
-
         int grainType;
 
         if (iG != -1 && jL != -1)
             if (frame[iG][jL].getState() == State.GRAIN) {
                 grainType = frame[iG][jL].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (iG != -1)
             if (frame[iG][j].getState() == State.GRAIN) {
                 grainType = frame[iG][j].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (iG != -1 && jR != -1)
             if (frame[iG][jR].getState() == State.GRAIN) {
                 grainType = frame[iG][jR].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (jL != -1)
             if (frame[i][jL].getState() == State.GRAIN) {
                 grainType = frame[i][jL].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (jR != -1)
             if (frame[i][jR].getState() == State.GRAIN) {
                 grainType = frame[i][jR].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (iD != -1 && jL != -1)
             if (frame[iD][jL].getState() == State.GRAIN) {
                 grainType = frame[iD][jL].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (iD != -1)
             if (frame[iD][j].getState() == State.GRAIN) {
                 grainType = frame[iD][j].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
         if (iD != -1 && jR != -1)
             if (frame[iD][jR].getState() == State.GRAIN) {
                 grainType = frame[iD][jR].getId();
-                fillMap(grainType);
+                fillMap(grainType, grainMap);
             }
 
-        int max = getIDMaxNeighbour();
+        int max = getIDMaxNeighbour(grainMap);
         newID = max;
 
         return newID;
     }
 
-    private int randomPentagonal(CellGrain[][] frame, int height, int width) {
-        grainMap = new HashMap<>();
+    private int randomPentagonal(CellGrain[][] frame, int iG, int i, int iD, int jL, int j, int jR) {
+        Map<Integer, Integer> grainMap = new HashMap<>();
         int newID = 0;
+        int grainType;
 
+        int U = 0, R = 1, D = 2, L = 3;
+        Random rand = new Random();
+        int randDirection = rand.nextInt(4);
+
+        if ((iG != -1 && jL != -1) && (randDirection != U && randDirection != L))
+            if (frame[iG][jL].getState() == State.GRAIN) {
+                grainType = frame[iG][jL].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((iG != -1) && randDirection != U)
+            if (frame[iG][j].getState() == State.GRAIN) {
+                grainType = frame[iG][j].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((iG != -1 && jR != -1) && (randDirection != U && randDirection != R))
+            if (frame[iG][jR].getState() == State.GRAIN) {
+                grainType = frame[iG][jR].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((jL != -1) && randDirection != L)
+            if (frame[i][jL].getState() == State.GRAIN) {
+                grainType = frame[i][jL].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((jR != -1) && randDirection != R)
+            if (frame[i][jR].getState() == State.GRAIN) {
+                grainType = frame[i][jR].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((iD != -1 && jL != -1) && (randDirection != D && randDirection != L))
+            if (frame[iD][jL].getState() == State.GRAIN) {
+                grainType = frame[iD][jL].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((iD != -1) && randDirection != D)
+            if (frame[iD][j].getState() == State.GRAIN) {
+                grainType = frame[iD][j].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if ((iD != -1 && jR != -1) && (randDirection != D && randDirection != R))
+            if (frame[iD][jR].getState() == State.GRAIN) {
+                grainType = frame[iD][jR].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        int max = getIDMaxNeighbour(grainMap);
+        newID = max;
 
         return newID;
     }
 
-    private int randomHexagonal(CellGrain[][] frame, int height, int width) {
-        grainMap = new HashMap<>();
+    private int randomHexagonal(CellGrain[][] frame, int iG, int i, int iD, int jL, int j, int jR) {
+        Map<Integer, Integer> grainMap = new HashMap<>();
         int newID = 0;
+        int grainType;
 
+        if (iG != -1)
+            if (frame[iG][j].getState() == State.GRAIN) {
+                grainType = frame[iG][j].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if (jL != -1)
+            if (frame[i][jL].getState() == State.GRAIN) {
+                grainType = frame[i][jL].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if (jR != -1)
+            if (frame[i][jR].getState() == State.GRAIN) {
+                grainType = frame[i][jR].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        if (iD != -1)
+            if (frame[iD][j].getState() == State.GRAIN) {
+                grainType = frame[iD][j].getId();
+                fillMap(grainType, grainMap);
+            }
+
+        Random rand = new Random();
+
+        if (rand.nextInt(2) == 0) {
+            if (iG != -1 && jL != -1)
+                if (frame[iG][jL].getState() == State.GRAIN) {
+                    grainType = frame[iG][jL].getId();
+                    fillMap(grainType, grainMap);
+                }
+
+            if (iD != -1 && jR != -1)
+                if (frame[iD][jR].getState() == State.GRAIN) {
+                    grainType = frame[iD][jR].getId();
+                    fillMap(grainType, grainMap);
+                }
+        } else {
+            if (iG != -1 && jR != -1)
+                if (frame[iG][jR].getState() == State.GRAIN) {
+                    grainType = frame[iG][jR].getId();
+                    fillMap(grainType, grainMap);
+                }
+
+            if (iD != -1 && jL != -1)
+                if (frame[iD][jL].getState() == State.GRAIN) {
+                    grainType = frame[iD][jL].getId();
+                    fillMap(grainType, grainMap);
+                }
+        }
+
+        int max = getIDMaxNeighbour(grainMap);
+        newID = max;
 
         return newID;
     }
@@ -246,73 +300,44 @@ public class ModelGrainGrowth {
     private int checkNeighbours(CellGrain[][] frame, int height, int width) {
         int result = 0;
 
+        int iG, i, iD, jL, j, jR;
+        i = height;
+        j = width;
+
+        if (height == 0)
+            if (edgeType == EdgeType.Closed) iG = -1;
+            else iG = gridHeight - 1;
+        else iG = height - 1;
+
+        if (height == gridHeight - 1)
+            if (edgeType == EdgeType.Closed) iD = -1;
+            else iD = 0;
+        else iD = height + 1;
+
+        if (width == 0)
+            if (edgeType == EdgeType.Closed) jL = -1;
+            else jL = gridWidth - 1;
+        else jL = width - 1;
+
+        if (width == gridWidth - 1)
+            if (edgeType == EdgeType.Closed) jR = -1;
+            else jR = 0;
+        else jR = width + 1;
+
         switch (neighborhoodType) {
             case vonNeuman:
-                result = vonNeuman(frame, height, width);
+                result = vonNeuman(frame, iG, i, iD, jL, j, jR);
                 break;
             case Moore:
-                result = Moore(frame, height, width);
+                result = Moore(frame, iG, i, iD, jL, j, jR);
                 break;
             case randomPentagonal:
-                result = randomPentagonal(frame, height, width);
+                result = randomPentagonal(frame, iG, i, iD, jL, j, jR);
                 break;
             case randomHexagonal:
-                result = randomHexagonal(frame, height, width);
+                result = randomHexagonal(frame, iG, i, iD, jL, j, jR);
                 break;
         }
-
-//        int iG, i, iD, jL, j, jR;
-//        i = height;
-//        j = width;
-//
-//        if (height == 0) iG = gridHeight - 1;
-//        else iG = height - 1;
-//
-//        if (height == gridHeight - 1) iD = 0;
-//        else iD = height + 1;
-//
-//        if (width == 0) jL = gridWidth - 1;
-//        else jL = width - 1;
-//
-//        if (width == gridWidth - 1) jR = 0;
-//        else jR = width + 1;
-//
-//        int grainType;
-//
-//            if (frame[iG][jL].getState() == State.GRAIN) {
-//                grainType = frame[iG][jL].getId();
-//                fillMap(grainType);
-//            }
-//            if (frame[iG][j].getState() == State.GRAIN){
-//                grainType = frame[iG][j].getId();
-//                fillMap(grainType);
-//            }
-//            if (frame[iG][jR].getState() == State.GRAIN){
-//                grainType = frame[iG][jR].getId();
-//                fillMap(grainType);
-//            }
-//
-//            if (frame[i][jL].getState() == State.GRAIN){
-//                grainType = frame[i][jL].getId();
-//                fillMap(grainType);
-//            }
-//            if (frame[i][jR].getState() == State.GRAIN){
-//                grainType = frame[i][jR].getId();
-//                fillMap(grainType);
-//            }
-//
-//            if (frame[iD][jL].getState() == State.GRAIN){
-//                grainType = frame[iD][jL].getId();
-//                fillMap(grainType);
-//            }
-//            if (frame[iD][j].getState() == State.GRAIN){
-//                grainType = frame[iD][j].getId();
-//                fillMap(grainType);
-//            }
-//            if (frame[iD][jR].getState() == State.GRAIN){
-//                grainType = frame[iD][jR].getId();
-//                fillMap(grainType);
-//            }
 
         return result;
     }
